b0175c9bf3a972e975b2d543b2470eb898bafbb5
Nikolaos Kavvadias <kavvadias@silexica.com>
2017-01-08 14:42:52 +0100

    More double frees now for pfm

--- src/rnwimg.c
+++ src/rnwimg.c
@@ -194,10 +194,12 @@ int main(int argc, char **argv)
 
   if (enable_pfm == 1) {
     free(pfm_data);
-  } else {
+  } else if (pnm_type == PBM_ASCII || pnm_type == PBM_BINARY ||
+    pnm_type == PGM_ASCII || pnm_type == PGM_BINARY || 
+    pnm_type == PPM_ASCII || pnm_type == PPM_BINARY) {
     free(img_data);
   }
-  free(imgin_file);
   free(imgin_file_name);
+
   return 0;
 }
