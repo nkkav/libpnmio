bdd86d0486bb33f18aea140798f2c8942e9c1e12
Nikolaos Kavvadias <kavvadias@silexica.com>
2019-11-19 14:58:34 +0100

    Bump up year

--- src/pnmio.c
+++ src/pnmio.c
@@ -2,7 +2,8 @@
  * File       : pnmio.c                                                          
  * Description: I/O facilities for PBM, PGM, PPM (PNM) binary and ASCII images.
  * Author     : Nikolaos Kavvadias <nikolaos.kavvadias@gmail.com>                
- * Copyright  : (C) Nikolaos Kavvadias 2012, 2013, 2014, 2015, 2016, 2017
+ * Copyright  : (C) Nikolaos Kavvadias 2012, 2013, 2014, 2015, 2016, 2017,
+ *                                     2018, 2019
  * Website    : http://www.nkavvadias.com                            
  *                                                                          
  * This file is part of libpnmio, and is distributed under the terms of the  
